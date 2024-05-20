5fef241983e4d903084a769f6f3ad76688fa1dcd
huygamcha <lehuynhhuy2002@gmail.com>
2024-05-21 02:04:12 +0700

    homepage

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -4,6 +4,7 @@ import Dashboard from '../pages/Dashboard/Dashboard'
 import RegisterForm from '../pages/Dashboard/Auth/RegisterForm'
 import LoginForm from '../pages/Dashboard/Auth/LoginForm'
 import Home from '../pages/Home/Home'
+import HomePage from '../pages/Home/HomePage/HomePage'
 
 
 const router = createBrowserRouter([
@@ -12,8 +13,14 @@ const router = createBrowserRouter([
     element: <App/>,
     children: [
       {
-        path: 'home',
-        element: <Home/>
+        path: '',
+        element: <Home/>,
+        children: [
+          {
+            path: '/',
+            element: <HomePage />
+          }
+        ]
       },
       {
         path: 'dashboard',
